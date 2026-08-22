46ed443e646c79da27dc0e60908f5ad59d109b64
Domcol05 <domenico.colandrea007@gmail.com>
2026-08-22 17:12:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'zante-cache-v3'; // Incrementa questo numero a ogni modifica
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Installazione: scarica i nuovi file e forza l'attivazione immediata
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Attivazione: cancella tutte le vecchie cache e prende il controllo dei client
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Strategia Network First con fallback su Cache per avere sempre i dati freschi
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
